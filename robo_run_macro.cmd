f5176640ebe1f547a0527d03251d58f22cfc0f15
fczero <0128aikon@gmail.com>
2016-11-09 13:16:06 +0800

    comment out robocopy commands, need testing

--- robo_run_macro.cmd
+++ robo_run_macro.cmd
@@ -4,10 +4,11 @@ SET DB_PATH_J=SUT_JAP
 SET JAP_PATH=%FOLDERNAME%/JAPANESE
 SET ENG_PATH=%FOLDERNAME%/ENGLISH
 timeout 5
-call delete_dbs.bat
-ROBOCOPY ".\%DB_PATH_J%" "."
-call robot -d %JAP_PATH% --variablefile jap_variables_macro.py -i Common -L trace db_check_macro.robot
-call delete_dbs.bat
-ROBOCOPY ".\%DB_PATH_E%" "."
-call robot -d %ENG_PATH% --variablefile eng_variables_macro.py -i Common -L trace db_check_macro.robot
-call delete_dbs.bat
+REM check robocopy behavior with symbolic links
+REM call delete_dbs.bat
+REM ROBOCOPY ".\%DB_PATH_J%" "."
+REM call robot -d %JAP_PATH% --variablefile jap_variables_macro.py -i Common -L trace db_check_macro.robot
+REM call delete_dbs.bat
+REM ROBOCOPY ".\%DB_PATH_E%" "."
+REM call robot -d %ENG_PATH% --variablefile eng_variables_macro.py -i Common -L trace db_check_macro.robot
+REM call delete_dbs.bat
